8b9acecfb6b94711cca5394a6d6fdea00f4ebdc5
Thorin Butler <thorin.butler@gmail.com>
2016-02-24 13:32:42 -0600

    added controller to register a user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
-
+MyApp.post "/register_user" do
+  @user = User.new
+  @user.name = params["name"]
+  @user.email = params["email"]
+  @user.password = params["password"]
+  @user.save
+end
 # This controller is for all the CRUD operations related to a User.
