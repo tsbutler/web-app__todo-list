a8e1b39d36a0f5b4480d15da685dbd292cef1da2
Thorin Butler <thorin.butler@gmail.com>
2016-02-24 22:53:39 -0600

    Sketched in remaining CRUD functions for todos and restricted access to those functions if the user is not logged in.  Now on to debugging in the morning.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,32 +1,74 @@
 MyApp.get "/todos/new" do
-  @todos = Todo.new
-  @todos.title = params["title"]
-  @todos.description = params["description"]
-  @todos.completed = params["completed"]
-  @todos.user_id = params["user_id"]
-  @todos.save
-  erb :"todos/new"
+  @current_user = User.find_by_id(session["user_id"]) 
+    if @current_user != nil
+      @todos = Todo.new
+      @todos.title = params["title"]
+      @todos.description = params["description"]
+      @todos.completed = params["completed"]
+      @todos.user_id = params["user_id"]
+      @todos.save
+      erb :"todos/new"
+    else
+      erb :"users/log_in_first"
+    end
 end
 
 MyApp.get "/todos/list" do
-  @todos = Todo.all
-  erb :"todos/todo_list"
+  @current_user = User.find_by_id(session["user_id"]) 
+    if @current_user != nil
+      @todos = Todo.all
+      erb :"todos/todo_list"
+    else
+      erb :"users/log_in_first"
+    end
 end
 
 MyApp.get "/todos/edit_todo" do
-  @todo = Todo.find_by_id(session["user_id"])
-  erb :"todos/edit_todo"
+  @current_user = User.find_by_id(session["user_id"]) 
+    if @current_user != nil
+      @todo = Todo.find_by_id(session["user_id"])
+      erb :"todos/edit_todo"
+    else
+      erb :"users/log_in_first"
+    end
 end
 
 MyApp.post "/todos/processed" do
-  @todos = Todo.find_by_id(session["user_id"])
-  @todos.title = params["title"]
-  @todos.description = params["description"]
-  @todos.completed = params["completed"]
-  @todos.user_id = params["user_id"]
-  @todos.save
-  erb :"todos/new"
+  @current_user = User.find_by_id(session["user_id"]) 
+    if @current_user != nil
+      @todos = Todo.find_by_id(session["user_id"])
+      @todos.title = params["title"]
+      @todos.description = params["description"]
+      @todos.completed = params["completed"]
+      @todos.user_id = params["user_id"]
+      @todos.save
+      erb :"todos/processed"
+    else
+      erb :"users/log_in_first"
+    end
+end
+    
 end
 
+MyApp.get "/form_to_delete_todo" do
+  @current_user = User.find_by_id(session["user_id"]) 
+    if @current_user != nil
+      @todo = Todo.find_by_id(session["user_id"])
+      erb :"todos/delete_todo"
+    else
+      erb :"users/log_in_first"
+    end
+end
+
+MyApp.post "/deleted" do
+  @current_user = User.find_by_id(session["user_id"]) 
+    if @current_user != nil
+      @todo = Todo.find_by_id(session["user_id"])
+      @todo.delete
+      erb :"todos/deleted"
+    else
+      erb :"users/log_in_first"
+    end  
+end
 # This controller is for all the CRUD operations related to a Todo.
 
