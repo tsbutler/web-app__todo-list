c045ad08451cdae82fd30c8f76d81e1bb1fc5d8c
Thorin Butler <thorin.butler@gmail.com>
2016-02-24 22:39:09 -0600

    Added controller for processing edit for...working out bugs

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,10 @@
 MyApp.get "/todos/new" do
+  @todos = Todo.new
+  @todos.title = params["title"]
+  @todos.description = params["description"]
+  @todos.completed = params["completed"]
+  @todos.user_id = params["user_id"]
+  @todos.save
   erb :"todos/new"
 end
 
@@ -6,4 +12,21 @@ MyApp.get "/todos/list" do
   @todos = Todo.all
   erb :"todos/todo_list"
 end
+
+MyApp.get "/todos/edit_todo" do
+  @todo = Todo.find_by_id(session["user_id"])
+  erb :"todos/edit_todo"
+end
+
+MyApp.post "/todos/processed" do
+  @todos = Todo.find_by_id(session["user_id"])
+  @todos.title = params["title"]
+  @todos.description = params["description"]
+  @todos.completed = params["completed"]
+  @todos.user_id = params["user_id"]
+  @todos.save
+  erb :"todos/new"
+end
+
 # This controller is for all the CRUD operations related to a Todo.
+
